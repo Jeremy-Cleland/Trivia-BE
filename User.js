c3ed53fb42ac505437c80414cb7b4b8cf5df278b
Brandon Perard <Brandon.Perard@Gmail.com>
2023-02-02 09:56:32 -0800

    Updated schema for FE functionality and refactored code

--- User.js
+++ User.js
@@ -1,23 +1,16 @@
-'Use Strict';
+'use strict';
+
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
 // schema is the structure of the document
 const userSchema = new Schema({
-  account: { type: String, required: true },
-  name: { type: String, required: false },
-  picture: { type: String, required: false },
-  location: { type: String, required: false },
+  _id: { type: String, required: true },
+  picture: { type: String, required: true },
+  score: { type: String, required: true },
 });
-// book is the name of the collection
+
+// user is the name of the collection
 const UserModel = mongoose.model('user', userSchema);
-
-
-
-
-
-
-
-
-
 
 module.exports = UserModel;
--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 'use strict'
+
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
+const cors = require('cors');
 
 const User = require('./User.js');
 const mongoose = require('mongoose');
-// mongoose.connect(process.env.DB_URL);
 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//   console.log('Mongoose is connected');
-// });
-
-const PORT = process.env.PORT || 3001;
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+// ENDPOINTS
 app.get('/test', (req, res) => {
   res.send('server is running');
 });
@@ -26,8 +21,7 @@ app.get('/users', getUsers);
 
 async function getUsers(req, res, next) {
   try {
-    let allUsers = await Users.find({});
-    
+    let allUsers = await User.find({});
 
     res.status(200).send(allUsers);
 
@@ -36,14 +30,13 @@ async function getUsers(req, res, next) {
     next(error);
   }
 }
+
 app.get('/users/:id', getSingleUser);
   
-
 async function getSingleUser(req, res, next) {
   try {
     let id = req.params.id;
-    let user = await Users.findById(id);
-    
+    let user = await User.findById(id); 
 
     res.status(200).send(user);
 
@@ -52,13 +45,13 @@ async function getSingleUser(req, res, next) {
     next(error);
   }
 }
-app.post('/users', newUsers); 
 
-async function newUsers(req, res, next) {
+app.post('/users', newUser); 
+
+async function newUser(req, res, next) {
   try {
     let data = req.body;
-    let newUsers = await Users.create(data);
-    
+    let newUsers = await User.create(data);
 
     res.status(200).send(newUsers);
 
@@ -67,14 +60,14 @@ async function newUsers(req, res, next) {
     next(error);
   }
 }
-app.delete('/users/:id', deletedUsers); 
 
-async function deletedUsers(req, res, next) {
+app.delete('/users/:id', deleteUser); 
+
+async function deleteUser(req, res, next) {
   try {
     let id = req.params.id;
-   await Users.findByIdAndDelete(id);
+   await User.findByIdAndDelete(id);
     
-
     res.status(200).send('users deleted');
 
   } catch (error) {
@@ -82,14 +75,14 @@ async function deletedUsers(req, res, next) {
     next(error);
   }
 }
-app.put('/users/:id',updateUser);
 
+app.put('/users/:id',updateUser);
 
 async function updateUser(req, res, next) {
   try {
     let id = req.params.id;
     let userData = req.body;
-    let updatedUser = await Users.findByIdAndUpdate(id, userData, {new: true, overwrite: true});
+    let updatedUser = await User.findByIdAndUpdate(id, userData, {new: true, overwrite: true});
     
 
     res.status(200).send(updatedUser);
@@ -99,14 +92,24 @@ async function updateUser(req, res, next) {
     next(error);
   }
 }
+
+// ERROR ENDPOINTS
 app.get('*', (req, res) => {
   res.status(404).send('Not Found');
 });
 
-// ERROR
 app.use((error, req, res, next) => {
   res.status(500).send(error.message);
 });
 
+// mongodb connection
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
 
+// server connection
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
